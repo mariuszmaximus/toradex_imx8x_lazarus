5a321bce1bc8c395bd818aad531d34651ef7955a
max <max@draminski.com>
2022-02-09 21:51:47 +0100

    sample project

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # toradex_imx8x_lazarus
 sample project and libs for toradex imx8x lazarus project 
+
+Apply patch   lazgtk3.pas.patch  it remove missing functions:  gtk_plug_..., gtk_socket_...,  
---
+++ sample_project/unit1.pas
@@ -0,0 +1,35 @@
+unit Unit1;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    Button1: TButton;
+    Button2: TButton;
+    Button3: TButton;
+    CheckBox1: TCheckBox;
+    Label1: TLabel;
+    RadioButton1: TRadioButton;
+  private
+
+  public
+
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+end.
+
